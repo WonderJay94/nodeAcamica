5506dc4eb2d46f1672899e76299f49d0ff20dcf2
WonderJay94 <j.jotamantilla@gmail.com>
2020-08-06 20:49:15 -0500

    Modulos de resta, multiplicacion y division

--- Calc/app.js
+++ Calc/app.js
@@ -1,4 +1,2 @@
 const fs = require('fs');
 const calc = require('./calculator');
-
-console.log(calc.add(5, 6));
--- Calc/calculator.js
+++ Calc/calculator.js
@@ -1,7 +1,18 @@
 function add(a,b){
     return a + b;
 }
-
+function subs(a, b){
+    return a - b;
+}
+function multi(a, b){
+    return a * b;
+}
+function divide(a, b){
+    return a/b;
+}
 module.exports={
-    add
+    add,
+    subs,
+    multi,
+    divide
 }
